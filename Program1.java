49b6a4dc32f5970cb9f78c264821486b35d8899d
mrfabroa <eric.fabroa@ycdsbk12.ca>
2023-10-16 09:34:09 -0400

    class examples

--- Program1.java
+++ Program1.java
@@ -1,12 +1,28 @@
 class Program1 extends ConsoleProgram {
 
-  /**
-  * Description
-  * @author:
-  */
+/**
+*Given a number n, calculate the sum of the numbers from 1 to n.
+*
+* @author: E. Fabroa
+*
+*/
+
   
   public void run() {
-    
+
+    // initialize variables
+    int intN = readInt("Enter N:" );
+    int intSum = 0;  // set the accumulator variable
+
+    // accumulate sum
+    for (int i = 1; i <= intN; i++){
+
+      // update our accumulator variable
+      intSum = intSum + i;
+    }
+
+    // output result
+    System.out.println(intSum);
     
   }
 }
--- Program2.java
+++ Program2.java
@@ -2,10 +2,19 @@ class Program2 extends ConsoleProgram {
 
   /**
   * Description
-  * @author:
+  * A program that converts kilometers to miles from 10 to 100km, every 10km
+  * @author: E. Fabroa
   */
   
   public void run() {
-   
+
+    double dblMiles;
+
+
+    for (int intKm = 10; intKm <= 100; intKm += 10){
+      dblMiles = intKm * 0.621371;
+      System.out.println(intKm + "km --> " + dblMiles + "mi");
+
+    }
   }
 }
